1d351f9376dc691bb4df7401afa67209e533a60f
Keegan Parrotte <keegan.parrotte@gmail.com>
2026-01-15 14:57:15 -0500

    custom launcher setup

--- launcher.py
+++ launcher.py
@@ -4,27 +4,43 @@ IMPORTS
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 import webbrowser
 import os
+import sys
 
 """
 SETTINGS
 """
 
 NAME = "localhost"
-# regular pokes (outdated) at 8080
-# ub pokes at 8090
-# three kingdoms at 8091
-# Cam & Alex at 8092
-# Leah at 8094
-# FF1 & 2 at 8095
-# Dragons & Other Proxies at 8096
-# FF3 at 8097
-PORT = 8097
 DIRECTORY = os.getcwd()
+DEFAULT_PORT = 8080
+
+# Static mappings
+PORT_MAP = {
+    "pokes": 8070,
+    "cam": 8071,
+    "leah": 8072,
+    "proxies": 8080
+}
+
+# Generate FF1 through FF16 (8081 -> 8096)
+for i in range(1, 17):
+    PORT_MAP[f"ff{i}"] = 8080 + i
+
+# Determine Port
+if len(sys.argv) > 1:
+    key = sys.argv[1].lower()
+    if key in PORT_MAP:
+        PORT = PORT_MAP[key]
+    elif key.isdigit():
+        PORT = int(key)
+    else:
+        PORT = DEFAULT_PORT
+else:
+    PORT = DEFAULT_PORT
 
 """
 REQUEST HANDLER
 """
-
 
 class Handler(SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
@@ -36,13 +52,14 @@ START APP
 
 if __name__ == "__main__":
     webServer = HTTPServer((NAME, PORT), Handler)
-    print("Server started http://%s:%s" % (NAME, PORT))
+    print(f"Server started http://{NAME}:{PORT}")
 
     try:
-        webbrowser.open('http://localhost:'+str(PORT), new=2)
+        webbrowser.open(f'http://{NAME}:{PORT}', new=2)
         webServer.serve_forever()
     except KeyboardInterrupt:
         pass
 
     webServer.server_close()
     print("Server stopped.")
+    
